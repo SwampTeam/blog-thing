286048d9fb0fd982dbaf973d29e902fdb23ec510
Apolline Weirich <weirich_apolline@msn.com>
2019-03-19 12:55:35 +0100

    fixed article in message + 100 limit attempt

--- partials/message.php
+++ partials/message.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$shortStr = substr(['message'], 0, 100);
+$shortStr = substr('message', 0, 100);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
@@ -10,12 +10,22 @@ for ($count = 0; $count < count($result); $count++) {
 
 
 ?>
-        <p><?php echo $result[$count]['username']?></p>
-        <img src=" <?php echo $result[$count]['image']?>" alt="">
-        <h2><?php echo $result[$count]['title']?></h2>
-        <p><?php echo $result[$shortStr]['message']?></p>
-        <button type="button"  class="read-message"> <a href="./partials/full-mesage.php"> <p class="read"><?php echo $result[$count]['button']?></p></a></button>
-        
+
+
+<article class="new-message2">
+    <div> <p class="user"><?php echo $result[$count]['username']?></p>
+    </div>    
+    <div class="content-message">
+        <img src="<?php echo $result[$count]['image']?>" alt="" style="width:300px; height:300px">
+        <h2 class="title"><?php echo $result[$count]['title']?></h2>
+        <p class="message"><?php echo $result[$shortStr]['message']?></p>
+    </div>
+    <button type="button" class="read-message"> 
+            <a href="./partials/full-message.php"> <p class="read">
+            <?php  echo ("<button onclick=\"location.href='./partials/full-message.php'\">Read More</button>")?> 
+        </p></a></button>
+</article>
+
 
 <?php
         }
