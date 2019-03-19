6f364d9c3b5195a2dbeb5fe913bf459d33071485
Apolline Weirich <weirich_apolline@msn.com>
2019-03-19 12:31:03 +0100

    first test limit 100 char

--- partials/message.php
+++ partials/message.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+$shortStr = substr(['message'], 0, 100);
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 for ($count = 0; $count < count($result); $count++) {
@@ -11,7 +13,8 @@ for ($count = 0; $count < count($result); $count++) {
         <p><?php echo $result[$count]['username']?></p>
         <img src=" <?php echo $result[$count]['image']?>" alt="">
         <h2><?php echo $result[$count]['title']?></h2>
-        <p><?php echo $result[$count]['message']?></p>
+        <p><?php echo $result[$shortStr]['message']?></p>
+        <button type="button"  class="read-message"> <a href="./partials/full-mesage.php"> <p class="read"><?php echo $result[$count]['button']?></p></a></button>
         
 
 <?php
