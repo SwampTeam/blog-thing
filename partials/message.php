74422502a59b3d3b29faf9ebeba913dfde786216
adolfo <buylla.adolfo@hotmail.com>
2019-03-19 14:01:19 +0100

    add working message

--- partials/message.php
+++ partials/message.php
@@ -1,28 +1,33 @@
 <?php
 session_start();
 
-$shortStr = substr('message', 0, 100);
+include './dbconnect.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$SQL = $connection->prepare('SELECT * FROM messages');
+$SQL->execute();
+$SQL->setFetchMode(PDO::FETCH_ASSOC);
+$result = $SQL->fetchAll();
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
 for ($count = 0; $count < count($result); $count++) {
     if(is_array($result[$count]) == true){
 
-
+        $shortStr = mb_strimwidth($result[$count]['message'], 0, 100,'...');
 ?>
 
 
 <article class="new-message2">
-    <div> <p class="user"><?php echo $result[$count]['username']?></p>
+    <div> <p class="user"><?php echo $result[$count]['user_id']?></p>
     </div>    
     <div class="content-message">
-        <img src="<?php echo $result[$count]['image']?>" alt="" style="width:300px; height:300px">
+        <img src="<?php echo $result[$count]['image_path']?>" alt="" style="width:300px; height:300px">
         <h2 class="title"><?php echo $result[$count]['title']?></h2>
-        <p class="message"><?php echo $result[$shortStr]['message']?></p>
+        <p class="message"><?php echo $shortStr ?></p>
     </div>
     <button type="button" class="read-message"> 
             <a href="./partials/full-message.php"> <p class="read">
-            <?php  echo ("<button onclick=\"location.href='./partials/full-message.php'\">Read More</button>")?> 
+            <?php  echo "<submit onclick=\"location.href='./partials/full-message.php'\">Read More</submit>";?>
         </p></a></button>
 </article>
 
